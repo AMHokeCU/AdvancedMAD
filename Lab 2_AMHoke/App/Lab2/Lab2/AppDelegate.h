349eceee71ae56d221ab9cf9d57229d850ffa769
AMHokeCU <alho6633@colorado.edu>
2016-09-20 02:51:28 -0600

    Add files via upload

---
+++ Lab 2_AMHoke/App/Lab2/Lab2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab2
+//
+//  Created by Alexander Marshall Hoke on 9/20/16.
+//  Copyright © 2016 Alexander Marshall Hoke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab 2_AMHoke/App/Lab2/Lab2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab2
+//
+//  Created by Alexander Marshall Hoke on 9/20/16.
+//  Copyright © 2016 Alexander Marshall Hoke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
